c3efc6fd944bc57b3954410001330751d975c171
lcssos <lcs@hh-medic.com>
2015-07-13 16:29:01 +0800

    Initial commit

---
+++ src/main/java/cn/martin/controller/UserController.java
@@ -0,0 +1,24 @@
+package cn.martin.controller;
+
+import cn.martin.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lcssos on 15/7/13.
+ */
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/{id}")
+    public User view(@PathVariable("id") Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("zhang");
+        return user;
+    }
+}
